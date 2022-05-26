142a4c574086e62d4ee78e5544659df7cec2c6e4
Stark Programmer <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-05-27 02:22:36 +0530

    Add alt

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -2,12 +2,11 @@ import spinner from '../assets/spinner.gif'
 
 function Spinner() {
   return (
-    // <div className='spinner'>
     <img
       src={spinner}
+      alt='Loading...'
       style={{ width: '100px', margin: 'auto', display: 'block' }}
     />
-    // </div>
   )
 }
 export default Spinner
