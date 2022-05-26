dfed6f8fe40cc89209c847aad7e31ea82051f3b9
Stark Programmer <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-05-26 23:30:26 +0530

    Initial

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import RatingSelect from './RatingSelect'
+import Button from './shared/Button'
+import Card from './shared/Card'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+  const [rating, setRating] = useState(10)
+
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext)
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
+  }, [feedbackEdit])
+
+  const handleText = (element) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text != '' && text.trim().length < 10) {
+      setMessage('Text must be atleast 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+    setText(element.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+        addFeedback(newFeedback)
+      }
+    }
+
+    setText('')
+    setBtnDisabled(true)
+    setRating(10)
+  }
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit} action='text'>
+        <h2>How would you rate your service with us</h2>
+        <RatingSelect select={(e) => setRating(e)} />
+        <div className='input-group'>
+          <input
+            onChange={handleText}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button id='send' type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  )
+}
+export default FeedbackForm
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import Card from '../components/shared/Card'
+
+function AboutPage() {
+  return (
+    <Card>
+      <h1>About This Project</h1>
+      <p>This is a React app to leave feedback for a service or a product.</p>
+      <p>Version: 1.0.0</p>
+      <p>
+        <a href='/'>Back To Home</a>
+      </p>
+    </Card>
+  )
+}
+export default AboutPage
