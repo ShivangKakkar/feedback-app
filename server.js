58b84112292acbde1bde7a939ef2f05ada59481f
Stark Programmer <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-05-27 13:17:40 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+import jsonServer from 'json-server'
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 80
+
+server.use(middlewares)
+server.use(router)
+
+// json-server --watch db.json --port $PORT
+
+server.listen(port)
